570cac83df453f381d1b4fc20d12401bf2a5cbeb
GtGnzz <111307507+GtGnzz@users.noreply.github.com>
2022-09-24 15:49:02 +0700

    Update SciTermux.sh

--- SciTermux.sh
+++ SciTermux.sh
@@ -6,13 +6,13 @@
 clear
 sleep 0.5
 echo ""
-echo "  [VISIT https://bajetech.org TO LEARN MORE]"|lolcat
+echo "  [DONT FORGET SUBSCRIBE MY YOUTUBE CHANEL]"|lolcat
 echo "    _____   _____ _   _ _____ __  __ _____"|lolcat
 echo "   |_   _| |_   _| | | | ____|  \/  | ____|"|lolcat
 echo "     | |_____| | | |_| |  _| | |\/| |  _|"  |lolcat
 echo "     | |_____| | |  _  | |___| |  | | |___" |lolcat
-echo "     |_|     |_| |_| |_|_____|_|  |_|_____| V1.5"|lolcat
-echo "  [CREATED BY: AliByte01]"|lolcat
+echo "     |_|     |_| |_| |_|_____|_|  |_|_____| V2.0"|lolcat
+echo "  [CREATED BY: GtGnzz]"|lolcat
 sleep 0.5
 echo ""
 echo "You Are About To Switch Termux To Colorfully Mode."|lolcat
@@ -28,23 +28,23 @@ cd
 cd /$HOME
 rm //data/data/com.termux/files/usr/etc/bash.bashrc
 cd
-mv /data/data/com.termux/files/home/TermuxTheme/code/bash.bashrc //data/data/com.termux/files/usr/etc/bash.bashrc
+mv /data/data/com.termux/files/home/SciTermux/code/bash.bashrc //data/data/com.termux/files/usr/etc/bash.bashrc
 sleep 8.0
 echo "Completed, Your Termux Now Looking Awesome"|lolcat
 sleep 1.0
 echo ""
 echo ""
 echo "Please exit from the termux and open it again to see the changes."|lolcat
-echo "IF YOU ARE INTERESTED IN DONATION"|lolcat
-echo "YOU CAN DONATE SOME SMALL AMOUNT OF EITHER"|lolcat
-echo "SmartChain (BNB) OR SOLANA TO ME FROM THE"|lolcat
-echo "MENTIONED ADDRESSES AND THEIRS NETWORKS BELOW."|lolcat
-echo ""
-echo "SmartChain (BNB) BEP20 Address👇"|lolcat
-echo "NETWORK BNB BEP20: 0x4Bc302A6755ED2B4D2311578d62B59f455E31152"
-echo ""
-echo "Solana (SOL) SOLANA Address👇"|lolcat
-echo "NETWORK SOLOANA: FnF7MWm6YpXygBZNWCun4qZA9VNiqnVnhC5na1MjVfFP"
+echo "---------------------------------------------"|lolcat
+echo "---------------------------------------------"|lolcat
+echo "--------------------------------------------"|lolcat
+echo "-------------------------------------------"|lolcat
 echo ""
 echo ""
-echo "VISIT https://bajetech.org TO LEARN MORE."
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
